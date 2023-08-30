bdf459182c1761334b3a3e766bd7f920923723d2
Kevin Pedro <kpedro88@gmail.com>
2023-08-30 15:08:03 -0500

    loss from continued training

--- configs/curves.py
+++ configs/curves.py
@@ -1,6 +1,4 @@
 from collections import OrderedDict
 curves = OrderedDict([
-    ("AEV", {"leg": r"$V$", "folder": ["schan"]}),
-    ("MT", {"leg": r"$M_{\mathrm{T}}$", "folder": ["schan_MI_MT"]}),
-    ("MAOS", {"leg": r"$M_{\mathrm{MAOS}}$", "folder": ["schan_MI_MAOS"]}),
+    ("AEV", {"leg": r"$V$", "folder": ["dijetPair_aug29_5","dijetPair_aug29_6"]}),
 ])
