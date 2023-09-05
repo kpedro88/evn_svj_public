0a91fb08a0167f111acb5dc7a0cbb55191ba94e1
Kevin Pedro <kpedro88@gmail.com>
2023-09-05 09:45:53 -0500

    finally suppress tf warnings

--- imports.py
+++ imports.py
@@ -2,6 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
 import scipy.stats
 import uproot as up
 import awkward as ak
